7ad34cd9a04d4ccae3fb6d8eb343f41d80bafc00
Sravani Chintha <sravanichintha15@gmail.com>
2023-05-15 11:08:47 +0530

    vue-express-school

---
+++ server/Express.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+  app.get('/',(req,res)=>{
+    res.send("hello")
+  })
+
+app.listen(3009);
